4524e773314e0fe7c7bbf0abd116b24bba0638ae
cagkanoztas <cagkan.oztas@jengal.com>
2023-01-09 02:09:46 +0300

    NgRx: Lesson 33 - Implement auto Login Functionality -2

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,7 +4,6 @@ import {AuthRoutingModule} from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {EffectsModule} from '@ngrx/effects';
-import {AuthEffects} from './state/auth.effects';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
